84b15062e1012402c9c4072b3e6609f6f17332ca
Devcodino <devcodino@proton.me>
2024-10-17 09:46:54 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Alert } from "react-bootstrap";
+
 import "./App.css";
 
 function App() {
   return (
     <>
+      <Alert variant="info">This is a new Alert</Alert>
       <h1>Hello from MovieList</h1>
+      <Button variant="success">Click Me</Button>
     </>
   );
 }
